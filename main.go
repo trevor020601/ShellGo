2c7f65579b2c64d35d49355427565b6c4a8c0029
Trevor Karl <trevorkarl5@gmail.com>
2023-02-19 20:58:14 -0600

    Read and Execute Commands

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
 
+func main() {
+	cmdReader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+
+		// Reads from the keyboard
+		cmdInput, e := cmdReader.ReadString('\n')
+		if e != nil {
+			fmt.Fprintln(os.Stderr, e)
+		}
+
+		if e = execInput(cmdInput); e != nil {
+			fmt.Fprintln(os.Stderr, e)
+		}
+	}
+}
+
+func execInput(input string) error {
+	// Removes newline
+	input = strings.TrimSuffix(input, "\n")
+
+	cmd := exec.Command(input)
+
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
 }
